af882e5d04d3fb9c1bd7921d6f1c9a3cbbfd9583
NightFox001 <johnnyh961@gmail.com>
2021-02-25 23:39:31 -0600

    updated file name and updated header

--- 4353-fuel-app/components/Header.js
+++ 4353-fuel-app/components/Header.js
@@ -8,21 +8,28 @@ export const Header = () => {
     const [searchValue, setSearchValue] = useState('')
 
 const handleLogOut = () => {
-    router.push('/')
+    router.push('/login')
 }
 
 const goToGetQuote = () => {
-    router.push('/FuelQuote')
+    router.push('/getQuote')
+}
+
+const goToProfile = () => {
+    router.push('/profile')
+}
+
+const goToHelp = () => {
+    router.push('/profile')
 }
 
   return (
     <>
-
         <div class='text-7xl'>Company Name</div>
         <div class='m-8 flex  text-1xl bg-gray-500 '>
-            <button class="flex-1 bg-gray-300 hover:bg-gray-500">Profile</button>
+            <button class="flex-1 bg-gray-300 hover:bg-gray-500" onClick={goToProfile}>Profile</button>
             <button class="flex-1 bg-gray-300 hover:bg-gray-500" onClick={goToGetQuote}>Get Quote</button>
-            <button class="flex-1 bg-gray-300 hover:bg-gray-500">Help</button>
+            <button class="flex-1 bg-gray-300 hover:bg-gray-500" onClick={goToHelp}>Help</button>
             <button class="flex-1 bg-gray-300 hover:bg-gray-500" onClick={handleLogOut}>Log out</button>
         </div>
     </>
