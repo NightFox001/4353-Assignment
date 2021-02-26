42718c2dd6a09707440fe8fd93daefcc6ee7ae30
NightFox001 <johnnyh961@gmail.com>
2021-02-25 23:26:49 -0600

    fixed merged conflit

--- 4353-fuel-app/components/Header.js
+++ 4353-fuel-app/components/Header.js
@@ -11,13 +11,17 @@ const handleLogOut = () => {
     router.push('/')
 }
 
+const goToGetQuote = () => {
+    router.push('/FuelQuote')
+}
+
   return (
     <>
 
         <div class='text-7xl'>Company Name</div>
         <div class='m-8 flex  text-1xl bg-gray-500 '>
             <button class="flex-1 bg-gray-300 hover:bg-gray-500">Profile</button>
-            <button class="flex-1 bg-gray-300 hover:bg-gray-500">Get Quote</button>
+            <button class="flex-1 bg-gray-300 hover:bg-gray-500" onClick={goToGetQuote}>Get Quote</button>
             <button class="flex-1 bg-gray-300 hover:bg-gray-500">Help</button>
             <button class="flex-1 bg-gray-300 hover:bg-gray-500" onClick={handleLogOut}>Log out</button>
         </div>
