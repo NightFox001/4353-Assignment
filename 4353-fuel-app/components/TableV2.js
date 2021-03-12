faf3b486d8abe0c6b4b1d3a6b87b10e5ee1822cd
alexkandrews <alexanderkandrews@gmail.com>
2021-03-12 17:07:52 -0600

    Made quote history headers bold

--- 4353-fuel-app/components/TableV2.js
+++ 4353-fuel-app/components/TableV2.js
@@ -9,27 +9,27 @@ export const Quotebox = ({ quote }) => {
     <div className="relative bg-white mt-6 rounded-xl p-6 shadow-xl">
       <div className="flex flex-row items-center justify-between">
         <div className="mr-4 flex flex-col items-center">
-            <div style={{ fontSize: 12, marginBottom: 5 }}>Address</div>
+            <div style={{ fontWeight: 700, fontSize: 12, marginBottom: 5 }}>Address</div>
             <div>{quote.delivery_address}</div>
         </div>
         <div className="mr-4 flex flex-col items-center">
-            <div style={{ fontSize: 12, marginBottom: 5 }}>Order Date</div>
+            <div style={{ fontWeight: 700, fontSize: 12, marginBottom: 5 }}>Order Date</div>
             <div>{quote.date_requested}</div>
         </div>
         <div className="mr-4 flex flex-col items-center">
-          <div style={{ fontSize: 12, marginBottom: 5 }}>Delivered</div>
+          <div style={{ fontWeight: 700, fontSize: 12, marginBottom: 5 }}>Delivered</div>
           <div>{quote.date_delivered}</div>
         </div>
         <div className="mr-4 flex flex-col items-center">
-          <div style={{ fontSize: 12, marginBottom: 5 }}>Gallons</div>
+          <div style={{ fontWeight: 700, fontSize: 12, marginBottom: 5 }}>Gallons</div>
           <div>{quote.gallons}</div>
         </div>
         <div className="mr-4 flex flex-col items-center">
-          <div style={{ fontSize: 12, marginBottom: 5 }}>Rate</div>
+          <div style={{ fontWeight: 700, fontSize: 12, marginBottom: 5 }}>Rate</div>
           <div>{`$${quote.rate}\gal`}</div>
         </div>
         <div className="mr-4 flex flex-col items-center">
-          <div style={{ fontSize: 12, marginBottom: 5 }}>Total Cost</div>
+          <div style={{ fontWeight: 700, fontSize: 12, marginBottom: 5 }}>Total Cost</div>
           <div>{`$${quote.total_price}`}</div>
         </div>
       </div>
