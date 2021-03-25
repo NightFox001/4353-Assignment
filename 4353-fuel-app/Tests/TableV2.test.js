73a708cee0bc4b331bc5fbedcec6175253127b82
alexkandrews <alexanderkandrews@gmail.com>
2021-03-25 17:58:25 -0500

    Update TableV2.test.js

--- 4353-fuel-app/Tests/TableV2.test.js
+++ 4353-fuel-app/Tests/TableV2.test.js
@@ -6,24 +6,10 @@ import {
 import "@testing-library/jest-dom/extend-expect"
 import { ExpansionPanelActions } from "@material-ui/core";
 
-test("renders quotebox without crashing", () => {
-    const quote = {
-        userId: 1,
-        quote_id: 12,
-        delivery_address : "10880 Malibu Point",
-        date_requested: "1/11/2021",
-        date_delivered: "2/11/2021",
-        gallons: 200,
-        rate: 2,
-        total_price: 2000,
-    };
-    const { getByTestId } = render(<TableV2 quote={quote}/>);
-    const quoteBox = getByTestId("quotebox");
-    expect(quoteBox).toBeInTheDocument();
-});
+let expectedQuote;
 
-test("displays values from quote in quotebox", () => {
-    const quote = {
+beforeEach(() => {
+    expectedQuote = {
         userId: 1,
         quote_id: 12,
         delivery_address : "10880 Malibu Point",
@@ -33,7 +19,16 @@ test("displays values from quote in quotebox", () => {
         rate: 2,
         total_price: 2000,
     };
-    const { getByTestId, queryByText } = render(<TableV2 quote={quote}/>);
+})
+
+test("renders quotebox without crashing", () => {
+    const { getByTestId } = render(<TableV2 quote={expectedQuote}/>);
+    const quoteBox = getByTestId("quotebox");
+    expect(quoteBox).toBeInTheDocument();
+});
+
+test("displays values from quote in quotebox", () => {
+    const { getByTestId, queryByText } = render(<TableV2 quote={expectedQuote}/>);
     const quoteBox = getByTestId("quotebox");
     expect(queryByText("10880 Malibu Point")).toBeTruthy();
     expect(queryByText("1/11/2021")).toBeTruthy();
