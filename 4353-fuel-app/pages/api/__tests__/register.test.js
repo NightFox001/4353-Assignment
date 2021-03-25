fd4b3d714d9deaeb007c8309a216eeea1ba0743f
NightFox001 <johnnyh961@gmail.com>
2021-03-25 00:32:05 -0500

    added testing for register api

---
+++ 4353-fuel-app/pages/api/__tests__/register.test.js
@@ -0,0 +1,42 @@
+import register from "../register";
+
+describe("Register API", () => {
+  let req;
+  let res;
+
+  beforeEach(() => {
+    req = {
+      method: "POST",
+      query: {
+        username: "Ironman",
+        password: "iamironman",
+      },
+    };
+    res = {
+      status: jest.fn(() => res),
+      end: jest.fn(),
+      json: jest.fn(),
+    };
+  });
+
+  it("Should return 405 if method is not POST", async () => {
+    req.method = "GET";
+    const response = await register(req, res);
+    expect(res.status).toBeCalledWith(405);
+  });
+
+  it("Should return 400 and message if username is taken", async () => {
+    req.query.username = "Ironman";
+    const response = await register(req, res);
+    expect(res.status).toBeCalledWith(400);
+    expect(res.json).toBeCalledWith({ message: "Username is not available." });
+  });
+
+  it("Should return 200 and message if username is taken", async () => {
+    req.query.username = "JohnSmithUniqueNameForTesting";
+    const response = await register(req, res);
+    expect(res.status).toBeCalledWith(200);
+    expect(res.json).toBeCalledWith({ message: "Account Created!" });
+  });
+
+
