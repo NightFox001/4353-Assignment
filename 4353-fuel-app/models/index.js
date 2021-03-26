50ae351c517dec5ae82c29170897698f84fe1a2b
Night Fox <Johnnyh961@gmail.com>
2021-03-26 14:19:41 -0500

    Made register api submit to DB

--- 4353-fuel-app/models/index.js
+++ 4353-fuel-app/models/index.js
@@ -6,10 +6,16 @@ const creds = {
   password: "7f8fa3ad1d35e27664e9a2f530846099fbceb4b4d86a0c40303b8e90dae73db4",
   host: "ec2-52-7-115-250.compute-1.amazonaws.com",
   dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 };
 
-const { database, username, password, ...config } = creds;
+// const { database, username, password, ...config } = creds;
 
-const connection = new Sequelize(database, username, password, config);
+const connection = new Sequelize(creds);
 
 module.exports = { connection, Sequelize };
--- 4353-fuel-app/pages/api/register.js
+++ 4353-fuel-app/pages/api/register.js
@@ -26,34 +26,34 @@ const handler = async (req, res) => {
     const response = await connection.query(`
     SELECT * 
     FROM user_credentials
-    WHERE username = ${username}`);
+    WHERE username = '${username}'`);
+    console.log("Users matching that username" + response[0]);
 
-    // no customer exists with that username
-    if (response.length === 0) {
-      // inster customer into DB
-      connection.query(`
-      BEGIN;
-      INSERT INTO dbiqsajdg6jh7e.user_credentials (username, password)
-      VALUES ('${username}, '${hashedPassword})
-      ;
-      `);
+    // check if customer exists with that username
+    if (response[0].length === 0) {
+      try {
+        // insert username/password into DB
+        connection.query(
+          `BEGIN;
+        INSERT INTO user_credentials (username, hashed_password)
+        VALUES ('${username}', '${hashedPassword}');`
+        );
+        connection.query("COMMIT;");
+        return res.status(200).json({ message: "Account Created!" });
+      } catch (error) {
+        connection.query(`ROLLBACK;`);
+        console.log(
+          `\n\n\n\n(In api/register.js)\nWhile trying to insert new customer\nwe got this error... \n 
+            ${error}`
+        );
+        return res.status(500).json({ message: error.message });
+      }
     } else {
       // username taken
       return res.status(400).json({ message: "Username is not available." });
     }
   } catch (e) {
     console.log(e);
-  }
-
-  try {
-    // insert username/password into DB
-    return res.status(200).json({ message: "Account Created!" });
-  } catch (error) {
-    console.log(
-      `\n\n\n\n(In api/register.js)\nWhile trying to insert new customer\nwe got this error... \n 
-        ${error}`
-    );
-    return res.status(500).json({ message: error.message });
   }
 };
 
